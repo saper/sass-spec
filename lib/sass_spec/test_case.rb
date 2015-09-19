b639aaa229762d7a962082c6ac76bd7b89d38df6
Marcin Cieślak <saper@saper.info>
2015-09-19 00:29:33 +0000

    Allow testing for exitcode and stderr output
    If files "error" and "status" are existing
    in the test directory, do expect errors
    and test against them.
    
    Also create error and status files on --nuke.
    An error will be reported in this case anyway.
    
    Fixes: https://github.com/sass/sass-spec/issues/136

--- lib/sass_spec/test_case.rb
+++ lib/sass_spec/test_case.rb
@@ -1,8 +1,10 @@
 # This represents a specific test case.
 class SassSpec::TestCase
-  def initialize(input_scss, expected_css, style, clean, options = {})
+  def initialize(input_scss, expected_css, error_file, status_file, style, clean, options = {})
     @input_path = input_scss
     @expected_path = expected_css
+    @error_path = error_file
+    @status_path = status_file
     @output_style = style
     @clean_test = clean
     @options = options
@@ -28,6 +30,22 @@ class SassSpec::TestCase
     @expected_path
   end
 
+  def error_path
+    @error_path
+  end
+
+  def verify_stderr?
+    File.file?(@error_path)
+  end
+
+  def status_path
+    @status_path
+  end
+
+  def should_fail?
+    File.file?(@status_path)
+  end
+
   def todo?
     @input_path.to_s.include? "todo"
   end
@@ -36,7 +54,9 @@ class SassSpec::TestCase
     if @output
       return @output
     end
+
     stdout, stderr, status = engine.compile(@input_path, @output_style)
+
     if @clean_test
       cleaned = _clean_output(stdout)
     else
@@ -51,6 +71,18 @@ class SassSpec::TestCase
       @expected ||= _clean_output(output)
     else
       @expected ||= _norm_output(output)
+    end
+  end
+
+  def expected_error
+    @expected_error = File.read(@error_path, :encoding => "utf-8")
+  end
+
+  def expected_status
+    if should_fail?
+      @expected_status = File.read(@status_path, :encoding => "utf-8").to_i
+    else
+      @expected_status = 0
     end
   end
 
