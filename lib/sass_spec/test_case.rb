889cb8adf74bad9aecc195e27700f6441d50271f
Marcin Cieślak <saper@saper.info>
2015-09-21 12:43:06 +0000

    Introduce expected_output.todo markers
    If expected*.todo file is present (alongside of the .css file)
    this marks this test as "todo", which can be ignored with
    --ignore-todo.
    
    Therefore test which have some output formats passing,
    but not the other ones can have a more fine-grained "todo"
    granularity. Such tests should not be in the folders
    that contain "todo", since then a whole test may be skipped.
    
    Should fix: https://github.com/sass/sass-spec/issues/333

--- lib/sass_spec/test_case.rb
+++ lib/sass_spec/test_case.rb
@@ -6,6 +6,7 @@ class SassSpec::TestCase
     @output_style = style
     @clean_test = clean
     @options = options
+    @todo = (@input_path.to_s.include? "todo") || File.file?(@expected_path.sub(/\.css$/, ".todo"))
   end
 
   def name
@@ -29,7 +30,7 @@ class SassSpec::TestCase
   end
 
   def todo?
-    @input_path.to_s.include? "todo"
+    @todo 
   end
 
   def output
